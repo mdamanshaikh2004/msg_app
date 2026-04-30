2440b482d8f2ce3ddb515a432929c47527e59f37
mdamanshaikh2004 <amanshaikh2k22@gmail.com>
2026-05-01 01:34:47 +0530

    fix model import case issue

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const Post = require("./models/Post");
+const Post = require("./models/Post.js");
 
 const app = express();
 
